61ee90563ab2bb21915b2e22e2f2acd8abdb36f0
beatrizguerra28 <83786167+beatrizguerra28@users.noreply.github.com>
2022-08-11 08:53:33 -0300

    Update ex04FR.hs

--- ex04FR.hs
+++ ex04FR.hs
@@ -1,10 +1,10 @@
 
-euclides :: Int -> Int -> Int 
-euclides x y 
-    | x == y = x
-    | x > y = euclides (x - y) y
-    | otherwise = euclides x (y - x)
-
 main :: IO ()
 main = do
     print $ euclides 6 27
+
+euclides :: Int -> Int -> Int 
+euclides x y 
+      | x == y = x
+      | x > y = euclides (x - y) y
+      | otherwise = euclides x (y -x)
